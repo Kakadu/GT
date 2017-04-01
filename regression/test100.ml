bc93736f091ce0ce1011c84fcbf7ca2ab8d595ca
kakadu <kakadu.hafanana@gmail.com>
2017-04-01 14:05:57 +0300

    Add new build systems

--- regression/test100.ml
+++ regression/test100.ml
@@ -1,5 +1,5 @@
-@type ('a, 'b) t1 = Constructor of 'a * 'b with map
+@type ('a, 'b) t1 = Constructor of 'a * 'b with gmap
 @type ('a, 'b) t2 = ('a, 'b) t1
 
-let test =
-  GT.transform(t1) (fun a -> a) (fun b -> b) @map
+(* let test =
+  GT.transform(t1) (fun a -> a) (fun b -> b) @map *)
